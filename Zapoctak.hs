96cc23e60a42f3c48ca5c5e4a2b0137df32099f8
brocny <miso.belak@gmail.com>
2017-06-17 16:34:10 +0200

    Changed push to not need the two nodes

--- Zapoctak.hs
+++ Zapoctak.hs
@@ -1,4 +1,4 @@
-data Node a = Node { num :: Int, height :: Int, flow :: a }
+data Node a = Node { num :: Int, height :: Int, excess :: a }
 
 data Edge a = Edge { from :: Node a, to :: Node a, capacity :: a, reserve :: a }
 
@@ -13,26 +13,37 @@ source = Node 5 0 0
 gr = Graph source sink [source,sink,node1,node2,node3] [Edge source node1 5 5, Edge source node2 4 4, Edge node2 node1 6 6, Edge node1 node3 7 7, Edge node2 sink 3 3, Edge node3 sink 7 7]
 
 initializePreFlow :: (Num a) => Graph a -> Graph a
-initializePreFlow (Graph (Node n h e) si ns es) = (Graph (Node n (length ns) 10000) si (map (\(Node n l e) -> (Node n 0 0)) ns) (map (\(Edge f t c r) -> (Edge f t c c)) es)) 
+initializePreFlow (Graph (Node n h e) si ns es) = (Graph (Node n (length ns) 10000000) si (map (\(Node n l e) -> (Node n 0 0)) ns) (map (\(Edge f t c r) -> (Edge f t c c)) es)) 
 {-
 transferFlowFromSource :: (Num a) => Graph a -> Graph a
-transferFlowFromSource (Graph so si ns es) = (Graph newSo si newNs newEs)
-    where newEs = map (\(Edge f t c r)->(Edge f t c 0)) edgesFromSource
-	      newNs = map (\(Node n h e)->(Node n h (capacity $ head $ (filter (\(Edge f _ _)->f == n) edgesFromSource))) nodesConnectedToSource
-		  newSo = (Node ((num so) (height so) (sum capacity edgesFromSource)
-          where edgesFromSource = filter (num from == num source) edges
-	            nodesConnectedToSource = filter ((\(Node n h e) -> (any (\(Edge f t c r)->(num t == n) edgesFromSource)) nodes 
--}	
+transferFlowFromSource (Graph so si ns es) = 
+     
+-}
+
+nodesConnectedToSource :: Graph a -> [Node a]
+nodesConnectedToSource gr = map to (edgesFromSource gr)
+
+
+edgesFromSource :: Graph a -> [Edge a]
+edgesFromSource (Graph sr sn ns es) = filter (\e -> (num $ from $ e) == (num sr)) es
+
+
 
 {-
 addReverseEdges :: (Num a) => [Edge a] -> [Edge a]
 addReverseEdges [] = []
 addReverseEdges ((Edge f t c r):edges) = if (any (\(Edge f1 t1 _ _)->(num f1 == num t && num t1 == num f)) edges) then (Edge f t c r) : addReverseEdges edges
                                                                                                   else (Edge f t c r) : (Edge t f 0 0) : addReverseEdges edges
-																								  -}
-																			
+-}
+
+nodesWithExcess :: Graph a -> [Node a]
+nodesWithExcess (Graph sr sn ns _) = filter (\(Node n h f) -> h>0 && n /= num sr && n /= num sn) ns
+	
+	
 relabel :: Node a -> Node a
 relabel (Node n h e) = (Node n (h + 1) e)
 
-push (Graph source sink nodes edges) (Node n1 h1 e1) (Node n2 h2 e2) edge@(Edge f t c r) = Graph source sink (((Node n1 h1 (e1 - delta))) : (Node n2 h2 (e2 + delta)) : [n | n <- nodes, num n /= n1 && num n /= n2]) ((Edge f t c (r - delta)):(filter (\e -> (num $ from $ e) /= n1 ||(num $ to $ e) /= n2) edges))
-    where delta = min e1 (reserve edge)
+push :: (Num a, Ord a) => Graph a -> Edge a -> Graph a
+push (Graph source sink nodes edges) edge@(Edge f t c r) = Graph source sink (((Node (num f) (height f) ((excess f) - delta))) : (Node (num t) (height t)  ((excess t) + delta)) : [n | n <- nodes, num n /= (num f) && num n /= (num t)]) ((Edge f t c (r - delta)):(filter (\e -> (num $ from $ e) /= (num f) ||(num $ to $ e) /= (num t)) edges))
+    where delta = min (excess f) (reserve edge)
+	
